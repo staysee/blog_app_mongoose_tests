9d7a391e4987d4b143858d8173289b1e37fa09f3
Stacey Sugiono <sugiono.stacey@gmail.com>
2018-10-09 22:36:31 -0700

    fixing tests

--- test/test-blog.js
+++ test/test-blog.js
@@ -61,7 +61,8 @@ describe('Blog Posts API resource', function(){
 				.then(function(_res){
 					res = _res;
 					expect(res).to.have.status(200);
-					expect(res.body.posts).to.have.lengthOf.at.least(1);
+					console.info(res.body);
+					expect(res.body).to.have.lengthOf.at.least(1);
 					return BlogPost.count();
 				})
 				.then(function(count) {
